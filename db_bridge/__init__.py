f73105e572cba6ba4022fdc82333d0cbd7fdf148
Priyadarshi Rishiraj <p.rishiraj@gmail.com>
2025-05-23 03:09:23 +0530

    exposing other functions in db_utils

--- db_bridge/__init__.py
+++ db_bridge/__init__.py
@@ -3,6 +3,16 @@ from importlib.metadata import version
 __version__ = version("db-bridge")   # reads pyproject.toml metadata
 
 from .config import load_config
-from .db_utils import run_sql
+from .db_utils import (
+    run_sql,
+    get_column_values,
+    get_column_values_regexp,
+)
 
-__all__ = ["__version__", "load_config", "run_sql"]
+__all__ = [
+    "__version__",
+    "load_config",
+    "run_sql",
+    "get_column_values",
+    "get_column_values_regexp",
+]
