9031b0a7bbe4b56433a1c2ab13b1eda5207085b5
Rishi@Rekha <p.rishiraj@gmail.com>
2025-05-22 05:38:11 -0400

    __init__ and gitignore

---
+++ dbbridge/__init__.py
@@ -0,0 +1,8 @@
+from importlib.metadata import version
+
+__version__ = version("dbbridge")   # reads your pyproject.toml metadata
+
+from .config import load_config
+from .db_utils import run_sql
+
+__all__ = ["__version__", "load_config", "run_sql"]
